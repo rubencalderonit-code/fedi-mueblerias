d221aa745e1161fa677838d21222a38ae3f9e91f
rubencalderonit-code <rubencalderonit@gmail.com>
2026-04-17 11:16:31 -0600

    Add files via upload

--- detalle.js
+++ detalle.js
@@ -8,35 +8,51 @@ fetch('productos.json')
 
     document.getElementById('nombre').innerText = producto.nombre;
     document.getElementById('precio').innerText = producto.precio;
-    document.getElementById('descripcion').innerText = producto.descripcion || '';
-    document.getElementById('material').innerText = producto.material || '';
-    document.getElementById('medidas').innerText = producto.medidas || '';
+    document.getElementById('material').innerText = producto.material || 'No especificado';
+    document.getElementById('medidas').innerText = producto.medidas || 'No especificado';
 
     const img = document.getElementById('imagenProducto');
 
+    // IMAGEN INICIAL
     if (producto.imagen) {
       img.src = producto.imagen;
     }
 
+    // COLORES
     if (producto.colores) {
+
       img.src = producto.colores[0].imagen;
 
-      const contenedorColores = document.getElementById('colores');
+      const contenedor = document.getElementById('colores');
 
       producto.colores.forEach(c => {
-        const btn = document.createElement('button');
-        btn.innerText = c.nombre;
+        const colorBtn = document.createElement('div');
+        colorBtn.classList.add('color-circulo');
 
-        btn.onclick = () => {
+        // COLOR VISUAL (simulado)
+        if (c.nombre.toLowerCase().includes('chocolate')) {
+          colorBtn.style.background = '#4b2e2b';
+        } else if (c.nombre.toLowerCase().includes('roble')) {
+          colorBtn.style.background = '#b08968';
+        } else {
+          colorBtn.style.background = '#ccc';
+        }
+
+        colorBtn.title = c.nombre;
+
+        colorBtn.onclick = () => {
           img.src = c.imagen;
         };
 
-        contenedorColores.appendChild(btn);
+        contenedor.appendChild(colorBtn);
       });
+
+    } else {
+      document.getElementById('coloresContainer').style.display = 'none';
     }
 
     document.getElementById('btnWhats').href =
       `https://wa.me/528443435820?text=${encodeURIComponent(
-        `Hola, me interesa el producto ${producto.nombre}`
+        `Hola, me interesa el producto ${producto.nombre} ${producto.precio}`
       )}`;
   });
